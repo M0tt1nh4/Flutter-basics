4b420f4c86cee30f21faf16ccd38f314474f29e0
M0tt1nh4 <felipemotta949@gmail.com>
2025-03-06 22:12:54 -0300

    first commit

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2.0,
+      ),
+      borderRadius: BorderRadius.circular(5)
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255)
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount in USD',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                print('Clicado');
+              },
+              child: Text('Click Me!'),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
